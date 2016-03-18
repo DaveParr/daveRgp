ad81b75b74f00ad340e2c883412a78196c24af9b
DaveRGP <DavidRGParr@gmail.com>
2016-03-18 19:26:11 +0000

    copied KnitPost and rmd2md functions

---
+++ R/KnitPost.R
@@ -0,0 +1,17 @@
+#' simply knits .md to .rmd
+#'
+#' http://jfisher-usgs.github.io/
+#'
+#' @param input file name of .Rmd
+#' @param base.url some other thing
+#' @return .md into directory
+#' @author j Fisher
+KnitPost <- function(input, base.url = "/") {
+  require(knitr)
+  opts_knit$set(base.url = base.url)
+  fig.path <- paste0("figs/", sub(".Rmd$", "", basename(input)), "/")
+  opts_chunk$set(fig.path = fig.path)
+  opts_chunk$set(fig.cap = "center")
+  render_jekyll()
+  knit(input, envir = parent.frame())
+}
